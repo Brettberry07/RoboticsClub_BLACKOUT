0312b86d30623fb373242a427fc41d52dc6decc2
Brett Berry <Brettberry07@gmail.com>
2024-09-04 10:24:55 -0500

    Added a Cubic Curve for sensitivity and more orecise driving, thinking about being able to switch between curves.

--- include/drivetrain.hpp
+++ include/drivetrain.hpp
@@ -5,3 +5,5 @@ void driveTrain(char dirveScheme);
 void tankDrive();
 void arcadeDriveTwo();
 void arcadeDriveOne();
+int cubicCurve(int controllerInput);
+int controllerCurve(int controllerInput);
--- src/drivetrain.cpp
+++ src/drivetrain.cpp
@@ -17,8 +17,8 @@ void driveTrain(char driveScheme){
 
 //TODO: Add sensitivity curves (accelaration curves)
 void tankDrive(){
-    leftChassis.move(master.get_analog(ANALOG_LEFT_Y));
-    rightChassis.move(master.get_analog(ANALOG_RIGHT_Y));
+    leftChassis.move(cubicCurve(master.get_analog(ANALOG_LEFT_Y)));
+    rightChassis.move(cubicCurve(master.get_analog(ANALOG_RIGHT_Y)));
 }
 
 void arcadeDriveTwo(){
@@ -33,4 +33,17 @@ void arcadeDriveOne(){
     int turn = master.get_analog(ANALOG_LEFT_X);
     leftChassis.move(power+turn);
     rightChassis.move(power-turn);
+}
+
+int cubicCurve(int controllerInput){
+    //Get percentage of controller input cube value, then multiply back to 127
+    return ((controllerInput/127)*(controllerInput/127)*(controllerInput/127))/127;
+}
+
+int controllerCurve(int controllerInput, int power){
+    int total = 1;
+    for(int i=0; i<power; i++){
+        total *= (controllerInput/127);
+    }
+    return total/127;
 }
